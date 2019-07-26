ffad3d1928b8cd751c172403d59624aae8972abd
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-07-26 11:34:11 -0300

    recebendo e validando dados do formulário

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,13 @@
+const { validationResult } = require('express-validator');
+
 module.exports.iniciar = function(application, req, res) {
+  let dadosForm = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render("index", {validacao: errors.array()})
+    //return res.status(422).json({ errors: errors.array() });
+    return;
+  }
+  console.log("XXXXXXXX")
   res.render("chat");
 }
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,13 @@
+const { check } = require('express-validator');
+
 module.exports = (application) => {
-  application.post("/chat", (req, res) => {
-    application.app.controllers.chat.iniciar(application, req, res);
-  })  
+  application.post("/chat",
+    [ check('apelido').isLength({ min: 3, max: 5 }).withMessage('Apelido deve ter no entre 3 e 15 caracter') ],
+    (req, res) => {
+      application.app.controllers.chat.iniciar(application, req, res);
+    })
 
   application.get("/chat", (req, res) => {
     application.app.controllers.chat.iniciar(application, req, res);
-  }) 
+  })
 }
